48c393fb1953a0fbecd7947caeb0cabfc2908926
solji <solji.choi89@gmail.com>
2025-01-07 10:49:17 +0900

    add userKey1

--- src/main/java/com/ll/security_2025_01_06/domain/member/member/controller/ApiV1MemberController.java
+++ src/main/java/com/ll/security_2025_01_06/domain/member/member/controller/ApiV1MemberController.java
@@ -12,6 +12,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/api/v1/members")
 @RequiredArgsConstructor
@@ -88,5 +90,15 @@ public class ApiV1MemberController {
         Member member = rq.getActor();
 
         return new MemberDto(member);
+    }
+
+    // 테스트용, 임시 함수
+    // 이 함수가 실행되려면 로그인이 되어있어야 한다.
+    // 로그인이 잘 되었는지 확인하기 위해서 `SELECT * FROM MEMBER WHERE API_KEY = '로그인시_받은_API_KEY';` 가 수행된다.
+    // 그런데 여기서는 회원정보를 활용하지 않는다.
+    // 로그인 되었는지 확인하기 위해서  `SELECT * FROM MEMBER WHERE API_KEY = '로그인시_받은_API_KEY';` 가 수행되는 것은 살짝 무겁다는 생각이 든다.
+    @GetMapping("/userKey1")
+    public String userKey1() {
+        return UUID.randomUUID().toString();
     }
 }
